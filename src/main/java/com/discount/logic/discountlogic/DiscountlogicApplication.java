42ed1e4611111fc488db34315eae9002b273b566
LAPTOP-91UHOKAL\athit <athithya.prabath@gmail.com>
2023-05-29 14:55:39 +0530

    Initial commit

---
+++ src/main/java/com/discount/logic/discountlogic/DiscountlogicApplication.java
@@ -0,0 +1,13 @@
+package com.discount.logic.discountlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountlogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discount/logic/discountlogic/DiscountlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discount.logic.discountlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
